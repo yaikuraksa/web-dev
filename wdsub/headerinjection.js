f9e87b1b00ddcea70917c865f5fe64205616ed9d
yaikuraksa <student.ik@youngsvilleacademy.org>
2026-04-27 13:28:08 -0400

    Delete wdsub/headerinjection.js

--- wdsub/headerinjection.js
+++
@@ -1,8 +0,0 @@
-let elem = document.createElement("div");
-elem.innerHTML = "<link rel='stylesheet' href='https://yaikuraksa.github.io/web-dev/styles.css'>";
-document.body.prepend(elem);
-
-fetch("https://yaikuraksa.github.io/web-dev/globalheader.html")
-  .then((result) => result.text())
-  .then((text) => {elem.innerHTML = elem.innerHTML + text;})
-  .catch((e) => console.error(e));
